097213f83caea9e3ae1fdb424d0b4a98eedc43ad
sn6 <41805222@qq.com>
2019-04-21 22:14:16 +0800

    pizza
    pizza

---
+++ pizza/precache-manifest.c42891d5ac7f6c4862bc85aa6c487f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "24d1da137ebcbb82cf39",
+    "url": "/js/chunk-vendors.4d47524f.js"
+  },
+  {
+    "revision": "6f3a79fc3cf4e364553b",
+    "url": "/js/app.2db1a9e9.js"
+  },
+  {
+    "revision": "36463b2e8fa152c0c9815553fb91a815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6f3a79fc3cf4e364553b",
+    "url": "/css/app.d3d297c2.css"
+  }
+];
